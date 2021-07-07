31f2354265f20a4a6d82db3ae2ab9178674675b8
themisvr <varveristhemis@gmail.com>
2021-07-07 22:38:49 +0300

    [frontend]: added some basic components

--- front-end/dina/src/app/app.module.ts
+++ front-end/dina/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    WelcomePageComponent,
+    TimelineComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
