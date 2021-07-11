a163f9e62d7393df46da80fe8058284fdf58a0b3
George Liontos <sdi1600094@di.uoa.gr>
2021-07-11 12:21:22 +0300

    [back-end]: Added certificate files, configured cors for server

--- back-end/app.py
+++ back-end/app.py
@@ -2,10 +2,12 @@ import os
 import db
 import auth
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
