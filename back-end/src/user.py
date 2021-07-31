407c9a2812d24bdf7df2b1bab3b72e2961c0bdca
themisvr <varveristhemis@gmail.com>
2021-08-01 01:31:25 +0300

    [back-end]: added update user

--- back-end/src/user.py
+++ back-end/src/user.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from http_constants.status import HttpStatus
-from utils import make_response, make_response_error
-from db import User
+from utils import make_response, make_response_error, json_to_table
+from db import User, db
 
 bp = Blueprint("users", __name__, url_prefix="/users")
 
@@ -19,4 +19,35 @@ def get_users():
 
     users = User.query.all()
     return make_response(users)
+
+@bp.route("/email", methods=['PUT'])
+def update_user():
+    if request.args.get("email"):
+        email = request.args.get("email")
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            content = request.get_json()
+
+            email = content.get("email")
+            password = content.get("password")
+
+            if not email and not password:
+                return make_response_error("No information is provided", HttpStatus.BAD_REQUEST)
+
+            user = json_to_table(content, User)
+
+            try:
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                return make_response_error("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR)
+
+            return make_response(user)
+
+        else:
+            return make_response_error(f"User with email {email} not found", HttpStatus.NOT_FOUND)
+
+
+
 
