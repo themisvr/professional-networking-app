bec5f620b033c260624455ec1c296ae94a979ee0
themisvr <varveristhemis@gmail.com>
2021-09-09 00:22:36 +0300

    [back-end]: fixed error in backend

--- back-end/src/app.py
+++ back-end/src/app.py
@@ -1,4 +1,3 @@
-import admin
 import os
 import db
 import auth
@@ -33,6 +32,5 @@ def create_app(test_config=None):
     app.register_blueprint(posts.bp)
     app.register_blueprint(jobPosts.bp)
     app.register_blueprint(search.bp)
-    app.register_blueprint(admin.bp)
 
     return app
